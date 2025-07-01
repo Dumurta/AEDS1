8f57d1247d1f7b541ee2049bb0ef838003fafa48
Eduardo Murta De Abreu <158529465+Dumurta@users.noreply.github.com>
2025-06-30 21:17:19 -0300

    a

--- study/DynamicArrays.cpp
+++ study/DynamicArrays.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 
-struct vetorDinamico {
-    int* dados;
+struct vetorDinamico
+{
+    int *dados;
     int tamanho;
     int capacidade;
 };
 
-vetorDinamico* criarVetor(int capacidade_inicial)
+vetorDinamico *criarVetor(int capacidade_inicial)
 {
-    vetorDinamico* novo_vetor = new vetorDinamico;
+    vetorDinamico *novo_vetor = new vetorDinamico;
     /**
      * a variavel novo_vetor agora sabe o endereco da minha struct vetorDinamico
      */
@@ -34,15 +35,15 @@ vetorDinamico* criarVetor(int capacidade_inicial)
  * a funcao para destruirVetor deve receber como parametro o vetor a ser
  * destruido no caso (vetorDinamico* apagarVetor)
  */
-void destruirVetor( vetorDinamico* apagarVetor)
+void destruirVetor(vetorDinamico *apagarVetor)
 {
     delete[] apagarVetor->dados;
     delete apagarVetor;
 }
 
-void push_back(vetorDinamico* vetor, int valor)
+void push_back(vetorDinamico *vetor, int valor)
 {
-    if(vetor->tamanho == vetor->capacidade)
+    if (vetor->tamanho == vetor->capacidade)
     {
         int nova_capacidade = vetor->capacidade * 2;
         if (nova_capacidade == 0)
@@ -50,12 +51,12 @@ void push_back(vetorDinamico* vetor, int valor)
             nova_capacidade = 1;
         }
 
-        int* novos_dados = new int[nova_capacidade];
-        for(int i = 0; i <= vetor->tamanho - 1; i = i + 1)
+        int *novos_dados = new int[nova_capacidade];
+        for (int i = 0; i <= vetor->tamanho - 1; i = i + 1)
         {
             novos_dados[i] = vetor->dados[i];
         }
-        delete[] vetor -> dados;
+        delete[] vetor->dados;
         vetor->dados = novos_dados;
         vetor->capacidade = nova_capacidade;
     }
@@ -63,7 +64,7 @@ void push_back(vetorDinamico* vetor, int valor)
     vetor->tamanho++;
 }
 
-void pop_back(vetorDinamico* vetor)
+void pop_back(vetorDinamico *vetor)
 {
     if (vetor->tamanho > 0)
     {
@@ -71,40 +72,105 @@ void pop_back(vetorDinamico* vetor)
     }
 }
 
-void push_front(vetorDinamico* vetor, int valor)
+void push_front(vetorDinamico *vetor, int valor)
 {
-    if(vetor->tamanho == vetor->capacidade)
+    if (vetor->tamanho == vetor->capacidade)
     {
         int nova_capacidade = vetor->capacidade * 2;
-        if(nova_capacidade == 0)
+        if (nova_capacidade == 0)
         {
             nova_capacidade = 1;
         }
-        int* novos_dados = new int[nova_capacidade];
-        for(int i = 0; i < vetor->tamanho; i++)
+        int *novos_dados = new int[nova_capacidade];
+        for (int i = 0; i < vetor->tamanho; i++)
         {
             novos_dados[i] = vetor->dados[i];
         }
-        delete[] vetor-> dados;
+        delete[] vetor->dados;
         vetor->dados = novos_dados;
         vetor->capacidade = nova_capacidade;
     }
-    for(int i = vetor->tamanho; i > 0; i--)
+    for (int i = vetor->tamanho; i > 0; i--)
     {
-        vetor->dados[i] = vetor->dados[i-1];
+        vetor->dados[i] = vetor->dados[i - 1];
     }
     vetor->dados[0] = valor;
     vetor->tamanho++;
 }
 
-void pop_front(vetorDinamico* vetor)
+void pop_front(vetorDinamico *vetor)
 {
-    if(vetor->tamanho > 0 )
+    if (vetor->tamanho > 0)
     {
-        for(int i = 0; i < vetor->tamanho - 1; i++)
+        for (int i = 0; i < vetor->tamanho - 1; i++)
         {
             vetor->dados[i] = vetor->dados[i + 1];
         }
         vetor->tamanho--;
     }
+}
+
+void imprimirVetor(vetorDinamico *vetor)
+{
+    if (vetor == nullptr)
+    {
+        std::cout << "Vetor Nulo" << std::endl;
+        return;
+    }
+    std::cout << "tamanho do vetor: " << vetor->tamanho
+              << std::endl;
+    std::cout << "Capacidade do vetor: " << vetor->capacidade
+              << std::endl;
+
+    std::cout << "Dados: [ ";
+    for (int i = 0; i < vetor->tamanho; i++)
+    {
+        std::cout << vetor->dados[i] << " ";
+    }
+    std::cout << "]" << std::endl;
+}
+
+int main()
+{
+    vetorDinamico *meu_vetor;
+
+    meu_vetor = criarVetor(4);
+
+    std::cout << "ESTADO INICIAL" << std::endl;
+
+    imprimirVetor(meu_vetor);
+
+    std::cout << "TESTANDO PUSH BACK" << std::endl;
+
+    push_back(meu_vetor, 10);
+    push_back(meu_vetor, 20);
+    push_back(meu_vetor, 30);
+
+    imprimirVetor(meu_vetor);
+
+    std::cout << "TESTANDO REDIMENSIONAMENTO" << std::endl;
+
+    push_back(meu_vetor, 40);
+    imprimirVetor(meu_vetor);
+    push_back(meu_vetor, 50);
+    imprimirVetor(meu_vetor);
+
+    std::cout << "TESTANDO POP_BACK" << std::endl;
+
+    pop_back(meu_vetor);
+    imprimirVetor(meu_vetor);
+
+    std::cout << "TESTANDO PUSH_fRONT" << std::endl;
+    push_front(meu_vetor, 99);
+    imprimirVetor(meu_vetor);
+
+    std::cout << "TESTANDO POP_FRONT" << std::endl;
+    pop_front(meu_vetor);
+    imprimirVetor(meu_vetor);
+
+    std::cout << " LIMPANDO A MEMORIA " << std::endl;
+    destruirVetor(meu_vetor);
+
+    std::cout << "Vetor destruido. Programa encerrado" << std::endl;
+    return 0;
 }
